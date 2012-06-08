5018c7d85ad49306d7ef3514734fb03f09358741
Xavier Lesa <xavierlesa@gmail.com>
2012-06-08 00:00:43 -0300

    xUI, mejora en vars de tempaltes

--- nebulajs.js
+++ nebulajs.js
@@ -672,8 +672,8 @@ var xUI = (function(){
         for ( var i = 0; i < strobj.length; i++ ) {
             if ( obj.hasOwnProperty( strobj[i] ) || obj[ strobj[i] ]) {
                 // identifica si es un metodo del objeto o una propiedad
+                console.log(obj, strobj[i] )
                 if( $n.utils.isFunction( obj[ strobj[i] ] ) ){
-                    console.log(obj, strobj[i])
                     try{ 
                         obj = obj[ strobj[i] ]( obj )
                     }catch(E){
