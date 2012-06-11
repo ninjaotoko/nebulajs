7f1f45a48929d1a3fae77f82fa8b3789a92b7548
Xavier Lesa <xavierlesa@gmail.com>
2012-06-11 16:45:28 -0300

    xUI, mejor compatibilidad con Date

--- nebulajs.js
+++ nebulajs.js
@@ -582,7 +582,7 @@ var xUI = (function(){
             var dt = str.split(" ");
             var date = dt[0].split('-');
             var time = dt[1].split(':');
-            return new Date(data[0], date[1], date[2], time[0], time[1], parseInt(time[2]))
+            return new Date(date[0], date[1], date[2], time[0], time[1], parseInt(time[2]))
         }
 
         return {
